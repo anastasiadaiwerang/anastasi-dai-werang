d6f751291975def8236bcb74ac9bc2ae63567d38
anastasiadaiwer <128022040+anastasiadaiwerang@users.noreply.github.com>
2023-03-22 15:20:53 +0000

    biodata

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_tamplate
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>anastasia werang</p>"
+  return render_tamplate ("index.html")
 
 
 if __name__ == "__main__":
